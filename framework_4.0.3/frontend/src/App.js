b2b19183579b0c3d297cb0cdab2eb11aaac80eb2
Ron G <ronald.greeff@gmail.com>
2020-09-21 17:07:53 +0100

    App working

---
+++ framework_4.0.3/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+let container = document.getElementById('app');
+let component = <Root />;
+ReactDOM.render(component, container);
