43b49500e0a680ff3b2dc1dc39b8381ae14329f8
Ron G <ronald.greeff@gmail.com>
2020-11-05 15:14:57 +0000

    contact controller, view done. SMTP setup. Works

--- framework_4.0.3/app/Controllers/Contact.php
+++ framework_4.0.3/app/Controllers/Contact.php
@@ -1,53 +1,51 @@
 <?php namespace App\Controllers;
 
-// Import PHPMailer classes into the global namespace
 use CodeIgniter\Controller;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
-require __DIR__.'\..\..\vendor\autoload.php';
+// require __DIR__.'/../../vendor/autoload.php';
 
 class Contact extends Controller
 {
 
+  // public function send($value='')
+  // {
+  //   $mail = new PHPMailer(true);
+  //
+  //   $mail->isSMTP();// Set mailer to use SMTP
+  //   $mail->CharSet = "utf-8";// set charset to utf8
+  //   $mail->SMTPAuth = true;// Enable SMTP authentication
+  //   $mail->SMTPSecure = 'tls';// Enable TLS encryption, `ssl` also accepted
+  //
+  //   $mail->Host = $_ENV['mail.Host'];// Specify main and backup SMTP servers
+  //   $mail->Port = 587;// TCP port to connect to
+  //   $mail->SMTPOptions = array(
+  //       'ssl' => array(
+  //           'verify_peer' => false,
+  //           'verify_peer_name' => false,
+  //           'allow_self_signed' => true
+  //       )
+  //   );
+  //   $mail->isHTML(true);// Set email format to HTML
+  //
+  //   $mail->Username = $_ENV['mail.Username'];// SMTP username
+  //   $mail->Password = $_ENV['mail.Password'];// SMTP password
+  //
+  //   $mail->setFrom($_ENV['mail.From']);//Your application NAME and EMAIL
+  //   $mail->addAddress($_ENV['mail.To']);// Target email
+  //   $mail->Subject = 'MedWeb Contact';//Message subject
+  //   $mail->MsgHTML('HTML code');// Message body
+  //
+  //   $mail->send();
+  // }
+
   public function index()
   {
     echo view('contact');
-  }
-
-  public function send($value='')
-  {
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = $_ENV['mail.Host'];                     // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = $_ENV['mail.Username'];                 // SMTP username
-    $mail->Password   = $_ENV['mail.Password'];                 // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = $_ENV['mail.Port'];                     // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net');                       // Add a recipient
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
   }
 }
 
