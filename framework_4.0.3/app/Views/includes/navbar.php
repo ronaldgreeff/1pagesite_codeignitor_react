bf860e477983c36d5dbe1a045bd4f2fd5f1efc3b
Ron G <ronald.greeff@gmail.com>
2020-11-06 13:46:52 +0000

    sorted out the navbar

--- framework_4.0.3/app/Views/includes/navbar.php
+++ framework_4.0.3/app/Views/includes/navbar.php
@@ -2,8 +2,9 @@
 
   <div class="nav-wrapper container">
 
-    <a id="logo-container" href="#" class="brand-logo">
-      <img src=<?= base_url('assets/img/logo.svg') ?> />
+    <a id="logo-container" href="/" class="brand-logo">
+      <img id="nav-logo" src=<?= base_url('assets/img/logo.svg') ?> />
+      <p class="corp-font">medweb</p>
     </a>
 
     <ul class="right hide-on-med-and-down">
@@ -23,7 +24,24 @@
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
+  var options = '';
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var instances = M.Sidenav.init(elems, options);
   });
 </script>
+
+<style>
+  .corp-font {
+    margin: 0;
+    font-size: 5vw;
+    font-family: UbuntuTitling-Bold;
+  }
+  #logo-container {
+    display: flex;
+    /* align-items: center;
+    justify-content: center; */
+  }
+  #nav-logo {
+    width: 50px;
+  }
+</style>
